78bfa142d0f58c5b17501e7f90bf3edfcacee24c
Will Harris <will@greatlibrary.net>
2009-01-02 16:45:00 +0000

    Initial commit.
    Sourced from http://www.greatlibrary.net/downloads/longlines.cpp.

---
+++ longlines.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <fstream>
+using namespace std;
+
+#include <stdlib.h>
+
+const string SYNTAX =
+    "Usage: %{command} -n -w <width> -t <tab size> [files]";
+
+const string HELP = SYNTAX + "\n"
+	"-n : show line numbers\n"
+	"-w : set threshole\n"
+	"-t : set width for tab expansion\n";
+	
+
+const int DEFAULT_MAX = 78;
+const int DEFAULT_TAB_WIDTH = 4;
+
+int max = DEFAULT_MAX;
+int tab_width = DEFAULT_TAB_WIDTH;
+bool show_line_numbers = false;
+
+string replacetag(const string, const string, const string);
+
+list<string> filenames;
+
+int main(int argc, char** argv)
+{
+	string arg;
+
+	arg = *argv;
+
+	string syntax = replacetag(SYNTAX, "command", arg);
+	string help = replacetag(HELP, "command", arg);
+
+	argv++;
+	argc--;
+
+	while (argc)
+	{
+		arg = *argv;
+
+    	argv++;
+		argc--;
+
+        if (arg == "-w")	// width
+		{
+
+			if (!argc)
+			{
+				cerr << syntax << "\n";
+				return 2;
+			}
+
+			max = atoi(*argv);
+			argv++;
+			argc--;
+		}
+
+		else if (arg == "-t")	// tab width
+		{
+			if (!argc)
+			{
+				cerr << syntax << "\n";
+				return 2;
+			}
+
+			tab_width = atoi(*argv);
+			argv++;
+			argc--;
+		}
+
+		else if (arg == "-n")	// show line numbers
+		{
+			show_line_numbers = true;
+		}
+
+		else if (arg == "-h")	// help
+		{
+			cout << help << "\n";
+			return 0;
+		}
+
+		else if (arg[0] == '-')	// unknown switch
+		{
+			cerr << syntax << "\n";
+			return 2;
+		}
+
+		else
+			filenames.insert(filenames.end(), arg);
+	}
+
+	if (!filenames.size() )
+	{
+		cerr << syntax << "\n";
+		return 2;
+	}
+
+	list<string>::iterator i = filenames.begin();
+
+	string buf = "";
+	int count = 0;
+
+	while (i != filenames.end() )
+	{
+		string filename = *i;
+		int lineno = 0;
+
+		char chr = '\0';
+		int col = 0;
+
+		ifstream file;
+		file.open(filename.c_str() );
+
+		while (!file.eof() )
+		{
+			file.read(&chr,1);
+
+			if (chr == '\t')
+			{
+				buf += " ";
+				col++;
+				while (col % tab_width)
+				{
+					buf += " ";
+					col++;
+				}
+			}
+
+			else if (chr == '\n')
+			{
+				lineno++;
+				if (col > max)
+				{
+					cout << filename << ":";
+
+					if (show_line_numbers)
+						cout << lineno << ":";
+				
+					cout << buf << "\n";
+
+					count++;
+				}
+
+				col = 0;
+				buf = "";
+
+			}
+
+			else
+			{
+				buf += chr;
+				col++;
+			}
+		}
+
+		i++;
+	}
+
+	return count == 0;
+}
+
+
+string replacetag(const string src, const string tag, const string value)
+{
+	int strpos = src.find("%{" + tag + "}");
+
+	return src.substr(0, strpos) + value
+		+ src.substr(strpos + tag.length() + 3, src.length() - strpos - 2);
+}
